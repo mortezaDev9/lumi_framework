9f21b37125362b857a80a9be87de2dac6d461020
Morteza <morteza9.dev@gmail.com>
2023-02-27 21:27:44 +0330

    get Router instance as an argument and use it directly instead of instantiating it

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,10 @@
 declare(strict_types=1);
 
 use App\Router;
-use Illuminate\Container\Container;
 use App\Controllers\HomeController;
 use App\Controllers\UserController;
 
-return function (Container $container) {
-    $router = new Router($container);
-    
+return function (Router $router) {
     $router->get('/home', [HomeController::class, 'index'])
     ->get('/users', [UserController::class, 'index'])
     ->get('/user', [UserController::class, 'show']);
