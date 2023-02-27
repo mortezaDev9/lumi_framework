3af82fd56498e61612b136e67b328c0fdb307a62
Morteza <morteza9.dev@gmail.com>
2023-02-27 21:42:31 +0330

    improved router call

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,19 +2,21 @@
 
 declare(strict_types=1);
 
+use App\Router;
 use App\Application;
 use Illuminate\Container\Container;
 
 $container = new Container();
 $router    = require_once __DIR__ . '/../routes/web.php';
+$router    = $router(new Router($container));
 
-// Set the $method and $uri to null so when we can use CLI and don't get undefined array key error
+// Set the $method and $uri to null so we can use CLI and don't get undefined array key error
 $method = isset($_SERVER['REQUEST_METHOD']) ? strtolower($_SERVER['REQUEST_METHOD']) : null;
 $uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : null;
 
 $app = new Application(
     $container,
-    $router($container),
+    $router,
     [
         'method' => $method,
         'uri'    => $uri
