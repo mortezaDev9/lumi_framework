adfd5713a7e3d5d534f7d916b23986b248a2c4b7
Morteza <morteza9.dev@gmail.com>
2023-02-28 14:32:36 +0330

    added support for URL parameter

--- app/Router.php
+++ app/Router.php
@@ -10,56 +10,63 @@ use App\Exceptions\RouteNotFoundException;
 class Router
 {
     private array $routes = [];
-    
+
     public function __construct(private Container $container)
     {
     }
-    
+
     public function routes(): array
     {
         return $this->routes;
     }
-    
+
     public function register(string $requestMethod, string $route, callable|array $action): self
     {
-        $this->routes[$requestMethod][$route] = $action;
+        $routeRegex = preg_replace('/\{([^\}]*)\}/', '(?P<$1>[^\/]*)', $route);
+        
+        $this->routes[$requestMethod][$routeRegex] = $action;
         
         return $this;
     }
-    
+
     public function get(string $route, callable|array $action): self
     {
         return $this->register('get', $route, $action);
     }
-    
+
     public function post(string $route, callable|array $action): self
     {
         return $this->register('post', $route, $action);
     }
-    
+
     public function resolve(string $requestMethod, string $requestUri): mixed
     {
-        $route = explode('?', $requestUri)[0];
-        $action = $this->routes[$requestMethod][$route] ?? null;
-        
-        if (! $action) {
-            throw new RouteNotFoundException();
-        }
-        
-        if (is_callable($action)) {
-            return call_user_func($action);
-        }
-        
-        [$class, $method] = $action;
-        
-        if (class_exists($class)) {
-            $class = $this->container->get($class);
+        foreach ($this->routes[$requestMethod] as $route => $action) {
+            $regex = '#^' . $route . '$#';
+            $matches = [];
             
-            if (method_exists($class, $method)) {
-                return call_user_func_array([$class, $method], []);
+            if (preg_match($regex, $requestUri, $matches)) {
+                array_shift($matches);
+                $params = array_values(array_filter($matches, 'is_int', ARRAY_FILTER_USE_KEY));
+                
+                if (is_callable($action)) {
+                    return call_user_func_array($action, $params);
+                }
+
+                [$class, $method] = $action;
+
+                if (class_exists($class)) {
+                    $class = $this->container->get($class);
+
+                    if (method_exists($class, $method)) {
+                        return call_user_func_array([$class, $method], $params);
+                    }
+                }
+
+                throw new RouteNotFoundException();
             }
         }
-        
+
         throw new RouteNotFoundException();
     }
 }
