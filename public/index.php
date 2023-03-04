b84d7104aad0a0230ceb8a7c87d4e884e49184c6
Morteza <morteza9.dev@gmail.com>
2023-03-04 13:52:29 +0330

    run the app

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/path_constants.php';
-require_once __DIR__ . '/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->run();
