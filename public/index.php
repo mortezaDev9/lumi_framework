08b7e9228a278eff69552e581b5541c34671da05
Morteza <morteza9.dev@gmail.com>
2023-03-04 13:42:14 +0330

    WID

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/path_constants.php';
 require_once __DIR__ . '/../bootstrap/app.php';
